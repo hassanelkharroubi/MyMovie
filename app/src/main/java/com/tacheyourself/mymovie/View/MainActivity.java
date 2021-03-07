c04403c94c0ec3dc6da22f8e66dab0c6d6e2dcc1
Hassan <hassanelkharroubi1997@gmail.com>
2021-03-07 22:32:11 +0100

    add login to MainActivity.java

--- app/src/main/java/com/tacheyourself/mymovie/View/MainActivity.java
+++ app/src/main/java/com/tacheyourself/mymovie/View/MainActivity.java
@@ -2,11 +2,21 @@ package com.tacheyourself.mymovie.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.tacheyourself.mymovie.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+
+    private EditText mUsernameText;
+    private EditText mPasswordText;
+    private Button signInButton;
+    private Button signUpButton;
 
 
 
@@ -14,5 +24,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mUsernameText=findViewById(R.id.username);
+        mPasswordText=findViewById(R.id.passsword);
+        signInButton=findViewById(R.id.loginIn);
+        signUpButton=findViewById(R.id.signUp);
+
+        signInButton.setOnClickListener(this);
+        signUpButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if(signUpButton.getId()==view.getId()){
+            startActivity(new Intent(this,AdminActivity.class));
+
+            return;
+        }
+
+        if(signInButton.getId()==view.getId()){
+            startActivity(new Intent(this,MovieListActivity.class));
+
+            return ;
+        }
+
+
     }
 }
